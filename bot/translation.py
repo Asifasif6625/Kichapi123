534c4a9ff9ef5a6804532781a76c6c2cc3700d5c
Asifasif6625 <86577020+Asifasif6625@users.noreply.github.com>
2021-07-21 17:48:25 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -12,45 +12,19 @@ class Translation(object):
 </b>"""    
     
     HELP_TEXT = """
-<b><i><u>ഒന്നും പ്രതീക്ഷിക്കരുത് ഞാൻ ഹെല്പ് ചെയ്യില്ല!?</u></i></b>
+<b>ഒന്നും പ്രതീക്ഷിക്കരുത് ഞാൻ ഹെല്പ് ചെയ്യില്ല! ഞാൻ ഒരുകാര്യം ചോദിക്കട്ടേ.. നാണം ഇല്ലേ നിനക്ക് 😠?</b>
 
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
 
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
 @cinimakottaka_official
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ പേര്</b> : <code> Queen bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ സംവിധായകന്‍</b> : <b><i><a href="https://t.me/asifpmn">Asif_TG</a></i></b>
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➥ ഭാഷ</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ കണ്ടുപിടിച്ചത്</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ ആശാൻ</b> : <i><a href="https://t.me/malayalamvibe">Click Me</a></i>
 """
